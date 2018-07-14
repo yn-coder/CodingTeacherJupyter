0f3e54739dfcc6ae788aeaff7a51e1cb4e5a8fad
yn_coder <yn.coder@gmail.com>
2018-07-14 17:09:19 +0300

    debug

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# As mentioned in https://github.com/Microsoft/AzureNotebooks/issues/245,
+# it looks like nbextensions is a symbolic link to a non-existing directory
+# initially
+rm -rf /home/nbuser/.local/share/jupyter/nbextensions
+mkdir /home/nbuser/.local/share/jupyter/nbextensions
+
 git pull
 
 jupyter nbextension install coding_teacher_ext --user
