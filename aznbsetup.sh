48e341c9a8785142a61370afedbaade7f99ee410
yn_coder <yn.coder@gmail.com>
2018-07-20 16:47:43 +0300

    #6 #4 first variant of panel (doesn't work without help_panel)

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -7,3 +7,9 @@ git pull
 jupyter nbextension install coding_teacher_ext --user
 
 jupyter nbextension enable coding_teacher_ext/main --section='common' --user
+
+
+
+jupyter nbextension install virtual_teacher_panel --user
+
+jupyter nbextension enable virtual_teacher_panel\main --section='common' --user
