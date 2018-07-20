f434707d7e646d6c79d53a3123001a131a526026
yn-coder <yn.coder@gmail.com>
2018-07-20 14:20:19 +0000

    #6

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -12,4 +12,4 @@ jupyter nbextension enable coding_teacher_ext/main --section='common' --user
 
 jupyter nbextension install virtual_teacher_panel --user
 
-jupyter nbextension enable virtual_teacher_panel\main --section='common' --user
+jupyter nbextension enable virtual_teacher_panel/main --section='common' --user
