bc01ca0a216458ef4a1536ec8cbeeb1461226ef5
yn_coder <yn.coder@gmail.com>
2018-10-12 22:37:17 +0300

    #9 drop useless `cell_help/main.js`

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -7,9 +7,5 @@ git pull
 jupyter nbextension install coding_teacher_ext --user
 jupyter nbextension enable coding_teacher_ext/main --section='common' --user
 
-
 jupyter nbextension install virtual_teacher_panel --user
 jupyter nbextension enable virtual_teacher_panel/main --section='common' --user
-
-jupyter nbextension install cell_help --user
-jupyter nbextension enable cell_help/main --section='common' --user
